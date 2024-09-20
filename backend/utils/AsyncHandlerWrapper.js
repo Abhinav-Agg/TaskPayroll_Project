71eb60b123618942c942b810e87d214684f5a617
Abhinav Aggarwal <abhinavaggarwal1724@outlook.com>
2024-09-20 16:53:21 +0530

    Add utils method

---
+++ backend/utils/AsyncHandlerWrapper.js
@@ -0,0 +1,14 @@
+//using higher order function for async handler. By this method not requried to use try catch in every method.
+const asyncHandler = (func) => async (req, res, next) => {
+    try {
+        // basically we pass function in main function and use the parameter of return function beacuse we call the eargument's function inside the return fucntion.
+        await func(req, res, next);
+
+    } catch (error) {
+        res.status(500).send({
+            message : error.message,
+            Status : "Failure"
+        })
+    }
+}
+
