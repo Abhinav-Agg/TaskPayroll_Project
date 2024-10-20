5598b037bb64bbfd9eb45b818ea4871bdbefb1ae
Abhinav Aggarwal <abhinavaggarwal1724@outlook.com>
2024-10-20 16:57:51 +0530

    Add Logic for approve, Multiple approve request. Also add logic for get latest balance leave for approve request. Understand req.params and req.query. Add two req.params in the route.

--- backend/controllers/leave_controller.js
+++ backend/controllers/leave_controller.js
@@ -7,87 +7,85 @@ const { validateEmpwithEmpNumber, findEmpDetail, checkMiddlewareCurrentUser } = 
 const db = require("../db/dbModel");
 
 // This created by only Addmin and Hr-admins
-const createEmployeesLeave = asyncHandler(async(req, res) => {
+const createEmployeesLeave = asyncHandler(async (req, res) => {
     let currentDateTime = new Date();
-    
-    let {empnumber, grantedLeaves, leaveEffectiveFrom, leaveEfectiveTo  } = req.body;
 
-    let {UserId, Role} = req.user;
+    let { empnumber, grantedLeaves, leaveEffectiveFrom, leaveEfectiveTo } = req.body;
 
-    if(Role === "Manager" || Role === "Employee" || Role === "Operations") throw new ApiError(401, "You have not access to create Leaves");
+    let { UserId, Role } = req.user;
+
+    if (Role === "Manager" || Role === "Employee" || Role === "Operations") throw new ApiError(401, "You have not access to create Leaves");
 
     let currentSystemDate = new Date(currentDateTime.getTime() - currentDateTime.getTimezoneOffset() * 60000);  // With this logic we get current system's date.
 
     let empDetail = await validateEmpwithEmpNumber(empnumber);
 
-    if(empDetail.ErrorMsg) throw new ApiError(401, empDetail.ErrorMsg);
+    if (empDetail.ErrorMsg) throw new ApiError(401, empDetail.ErrorMsg);
 
-    let dataEmpLeaveExists = await EmployeeLeaves.findOne({where : {Empnumber : empDetail.Empnumber, IsActive : 1}});
+    let dataEmpLeaveExists = await EmployeeLeaves.findOne({ where: { Empnumber: empDetail.Empnumber, IsActive: 1 } });
 
-    if(dataEmpLeaveExists?.IsActive === 1 
-        && dataEmpLeaveExists?.Empnumber === empDetail.Empnumber 
-        && dataEmpLeaveExists?.LeavesEffectiveFrom === leaveEffectiveFrom  
+    if (dataEmpLeaveExists?.IsActive === 1
+        && dataEmpLeaveExists?.Empnumber === empDetail.Empnumber
+        && dataEmpLeaveExists?.LeavesEffectiveFrom === leaveEffectiveFrom
         && dataEmpLeaveExists?.LeavesEffectiveTo === leaveEfectiveTo) {
-            throw new ApiError(401, "This Employee Exists with Same Data");
-        }
+        throw new ApiError(401, "This Employee Exists with Same Data");
+    }
 
     let objEmpLeave = {
-        Empnumber : empDetail.Empnumber,
-        LeavesGranted : grantedLeaves,
-        LeavesEffectiveFrom : leaveEffectiveFrom,
-        LeavesEffectiveTo : leaveEfectiveTo,
-        IsActive : 1,
-        IsDeleted : 0,
-        CreatedBy : UserId,
-        CreatedAt : currentSystemDate,
-        UpdatedAt : currentSystemDate
+        Empnumber: empDetail.Empnumber,
+        LeavesGranted: grantedLeaves,
+        LeavesEffectiveFrom: leaveEffectiveFrom,
+        LeavesEffectiveTo: leaveEfectiveTo,
+        IsActive: 1,
+        IsDeleted: 0,
+        CreatedBy: UserId,
+        CreatedAt: currentSystemDate,
+        UpdatedAt: currentSystemDate
     }
 
     let addedLeaveDetailsForEmp = await EmployeeLeaves.create(objEmpLeave);
-    
+
     res.status(200)
         .json(new ApiResponse(201, addedLeaveDetailsForEmp));
 });
 
-// Employees
-const addLeaveRequest = asyncHandler(async(req, res) => {
-    // In this Employee send the request for leave.
-    // We will add the data in leaves table.
-    // we need to check leaves lies in effective from and effective to or not.
-    let {leaveType, leaveDays, balanceLeaves, leaveFrom, leaveTo, leaveReason} = req.body;
+// Every Employees
+const addLeaveRequest = asyncHandler(async (req, res) => {
+    let { leaveType, leaveDays, balanceLeaves, leaveFrom, leaveTo, leaveReason } = req.body;
 
-    let {UserId} = req.user;
+    let { UserId } = req.user;
 
     let empDetail = await findEmpDetail(UserId);
-    
-    if(empDetail.ErrorMsg) throw new ApiError(401, empDetail.ErrorMsg);
+
+    if (empDetail.ErrorMsg) throw new ApiError(401, empDetail.ErrorMsg);
 
     let empLeaveDetail = await EmployeeLeaves.findOne({
         where:
         {
             Empnumber: empDetail.Empnumber, IsActive: 1,
-            LeavesEffectiveFrom : {
-                [Op.lte] : new Date(leaveFrom)
+            LeavesEffectiveFrom: {
+                [Op.lte]: new Date(leaveFrom)
             },
-            LeavesEffectiveTo : {
-                [Op.gte] : new Date(leaveTo)
+            LeavesEffectiveTo: {
+                [Op.gte]: new Date(leaveTo)
             }
         }
     });
 
-    if(!empLeaveDetail) throw new ApiError(401, "Please contact your System Administrator for leaves expired or add leaves");
+    if (!empLeaveDetail) throw new ApiError(401, "Please contact your System Administrator for leaves expired or add leaves");
 
-    if(leaveDays > balanceLeaves) throw new ApiError(400, "Insufficient Balance Leaves");
+    if (leaveDays > balanceLeaves) throw new ApiError(400, "Insufficient Balance Leaves");
 
     let leaveDetails = {
-        EmployeeLeaveId : empLeaveDetail.EmployeeLeaveId,
-        LeaveType : leaveType,
-        LeaveDays : leaveDays,
-        LeaveFromDate : new Date(leaveFrom),
-        LeaveToDate : new Date(leaveTo),
-        LeaveReason : leaveReason,
-        BalanceLeaves : balanceLeaves,
-        CreatedBy : UserId
+        EmployeeLeaveId: empLeaveDetail.EmployeeLeaveId,
+        LeaveType: leaveType,
+        LeaveDays: leaveDays,
+        LeaveFromDate: new Date(leaveFrom),
+        LeaveToDate: new Date(leaveTo),
+        LeaveReason: leaveReason,
+        BalanceLeaves: balanceLeaves,
+        LeaveStatus: "REQ",
+        CreatedBy: UserId
     }
 
     let leaveReqDetails = await Leaves.create(leaveDetails);
@@ -95,76 +93,176 @@ const addLeaveRequest = asyncHandler(async(req, res) => {
     res.status(200).send(new ApiResponse(201, leaveReqDetails));
 });
 
-// Employees
-const showLeavesDetails = asyncHandler(async(req, res) => {
+// Every Employees -> This api shows only their details.
+const showLeavesDetails = asyncHandler(async (req, res) => {
 
     let currentUserDetails = checkMiddlewareCurrentUser(req);
 
     let empDetail = await findEmpDetail(currentUserDetails.UserId);
 
-    if(empDetail.ErrorMsg) throw new ApiError(401, empDetail.ErrorMsg);
-    
+    if (empDetail.ErrorMsg) throw new ApiError(401, empDetail.ErrorMsg);
+
     let getLeaveDetailsInstance = await Leaves.findAll({
-        attributes : ["LeaveType", "BalanceLeaves", "LeaveStatus", "LeaveReason", "LeaveFromDate", "LeaveToDate", "RejectedReason"],
-        include : [{
-            model : db.EmployeeLeaves,
-            attributes : ["Empnumber", "LeavesGranted"],
-            required : true,
-            where : {Empnumber : empDetail.Empnumber}
+        attributes: ["LeaveId", "EmployeeLeaveId", "LeaveType", "BalanceLeaves", "LeaveStatus", "LeaveDays", "LeaveReason", "LeaveFromDate", "LeaveToDate", "ApprovedBy", "RejectedBy", "RejectedReason"],
+        include: [{
+            model: db.EmployeeLeaves,
+            attributes: ["Empnumber", "LeavesGranted"],
+            required: true,
+            where: { Empnumber: empDetail.Empnumber }
         }]
     });
 
-    let leaveDetails = getLeaveDetailsInstance.map(instance => instance.get({plain : true}));
-    
-    // Need to add code when leaves have no details. Means Emp is new and need to add details.
+    let leaveDetails = getLeaveDetailsInstance.map(instance => instance.get({ plain: true }));
+
     return res.send(new ApiResponse(201, "details", leaveDetails));
 });
 
 // Manager and HR_Admin
-const approveLeaves = asyncHandler(async(req, res) => {
-    // Need to test this yet.....................
-    let {LeaveId} = req.params;
+const approveLeaves = asyncHandler(async (req, res) => {
+    let { leaveId, employeeLeaveId } = req.params;
 
     let currentUserDetails = checkMiddlewareCurrentUser(req);
 
-    if(currentUserDetails === "Employee") throw new ApiError(401, "You don't have access to approved leaves");
+    let { UserId, Role } = currentUserDetails;
 
-    let getLeaveDetails = await Leaves.findAll({
-        where : {LeaveStatus : "REQ"}
+    if (Role === "Employee") throw new ApiError(401, "You don't have access to approved leaves");
+
+    // By this we reduce the two calls in one call with OR condition.
+    let leaveDetails = await Leaves.findAll({
+        where : {
+            [Op.or] : [{LeaveStatus : "REQ", LeaveId: leaveId }, {LeaveStatus: "APV", EmployeeLeaveId: employeeLeaveId}]
+        },
+        order: [["UpdatedAt", "DESC"]] 
     });
 
-    if(!getLeaveDetails) return res.status(200).send(new ApiResponse(201, "No Leaves Pending for Approve"));
+    let getReqLeaveDetails = leaveDetails.find(leaves => leaves.LeaveStatus === "REQ");
 
-    let {BalanceLeaves} = getLeaveDetails;
+    if (!getReqLeaveDetails) return res.status(200).send(new ApiResponse(201, "No Leaves Pending for Approve"));
 
-    BalanceLeaves -= LeaveDays;
+    let getbalanceLeaveAPVDetails = leaveDetails.find(leaves => leaves.LeaveStatus === "APV");
+
+    let { BalanceLeaves, LeaveDays } = getReqLeaveDetails;
+
+    // Determine the updated balance
+    let newBalance;
+    if (getbalanceLeaveAPVDetails) {
+        newBalance = getbalanceLeaveAPVDetails.BalanceLeaves - LeaveDays;
+
+    } else {
+        // If no approved leave found, use the current requested leave balance
+        newBalance = BalanceLeaves - LeaveDays;
+    }
 
     let leaveUpdateStatus = await Leaves.update(
         {
-            LeaveStatus : "APV",
-            ApprovedBy : currentUserDetails.UserId,
-            BalanceLeaves,
-            ModifiedBy : currentUserDetails.UserId
+            LeaveStatus: "APV",
+            ApprovedBy: UserId,
+            BalanceLeaves: newBalance,
+            ModifiedBy: UserId
         },
         {
-            where : {LeaveId}
+            where: { LeaveId: leaveId }
         }
     );
 
-    if(leaveUpdateStatus[0] === 1)
+    if (leaveUpdateStatus[0] === 1)
         return res.status(200).send(new ApiResponse(201, "Leave Approved"));
 
-    // Need to test this yet.....................
+});
+
+// Manager -> Here manager also see details of other employees in manager setion leaves request module.
+const showRequestStatusLeaves = asyncHandler(async (req, res) => {
+    const { UserId } = req.user;
+
+    let empDetail = await findEmpDetail(UserId);
+
+    if (empDetail.EmpRole === "Employee") throw new ApiError(401, "You don't have access");
+
+    const reqStatusLeaveDetails = await Leaves.findAll({
+        attributes: ["LeaveId", "EmployeeLeaveId", "LeaveType", "LeaveStatus", "BalanceLeaves", "LeaveDays", "LeaveReason", "LeaveFromDate", "LeaveToDate"],
+        include: [{
+            model: db.EmployeeLeaves,
+            required: true,
+            attributes: [], // Exclude all EmployeeLeaves columns
+            where: {
+                Empnumber: {
+                    [Op.ne]: [empDetail.Empnumber]
+                }
+            }
+        }],
+        where: { LeaveStatus: "REQ" }
+    });
+
+    if (!reqStatusLeaveDetails) res.status(200).send(new ApiResponse(201, "No Pending Leaves"));
+
+    return res.status(200).json(new ApiResponse(201, "Leave Details", reqStatusLeaveDetails));
+});
+
+// Manager -> Allow approve Multiple leaves.
+const multipleApproveLeaves = asyncHandler(async (req, res) => {
+    let { checkedLeaveIdsData } = req.body;
+
+    let currentUserDetails = checkMiddlewareCurrentUser(req);
+
+    let { UserId } = currentUserDetails;
+
+    if (currentUserDetails.Role === "Employee") throw new ApiError(401, "You don't have access to approved leaves");
+
+    let empUpdatedBalanceLeaves = {};  // By this object we didn't require to fetch the details from backend. It saves a read call.
+
+    try {
+        let leaveUpdateStatuses = checkedLeaveIdsData.map((leaveDetail) => {
+            let { BalanceLeaves, LeaveDays, LeaveId, EmployeeLeaveId } = leaveDetail;
+
+            // By this we will catch same employeeLeaveId balance leave which already udpated.
+            if (empUpdatedBalanceLeaves["employeeLeaveId"] === EmployeeLeaveId) {
+                BalanceLeaves = empUpdatedBalanceLeaves["balanceLeave"] - LeaveDays;
+            }
+            else {
+                BalanceLeaves -= LeaveDays;
+            }
+
+            empUpdatedBalanceLeaves["balanceLeave"] = BalanceLeaves;
+            empUpdatedBalanceLeaves["employeeLeaveId"] = EmployeeLeaveId;
+            empUpdatedBalanceLeaves["leaveId"] = LeaveId;
+
+            // Each Leaves.update returns a promise
+            return Leaves.update(
+                {
+                    LeaveStatus: "APV",
+                    ApprovedBy: UserId,
+                    BalanceLeaves,
+                    ModifiedBy: UserId
+                },
+                {
+                    where: { LeaveStatus: "REQ", LeaveId }
+                }
+            );
+        });
+
+        // Await all promises concurrently
+        let updateStatus = await Promise.all(leaveUpdateStatuses);
+
+        let resUpdateStatus = updateStatus.some((status) => status[0] === 1);
+
+        if (!resUpdateStatus) throw new ApiError(401, "Something went wrong");
+
+        return res.status(200).send(new ApiResponse(201, "Leave Approved Successfully"));
+
+    } catch (error) {
+        console.error('Error in updating leaves:', error);
+    }
+
 });
 
 // Manager
-const rejectLeaves = asyncHandler(async(req, res) => {
+const rejectLeaves = asyncHandler(async (req, res) => {
     // Here Manager will reject the leaves.
     // Will do calcualtion for balance leaves when manager rejected the leaves. We need to add those leaves in balance leaves.
 });
 
 // Employees
-const withdrawLeaves = asyncHandler(async(req, res) => {
+const withdrawLeaves = asyncHandler(async (req, res) => {
     // Here Empoyee will withdraw the leaves.
     // System will add these leaves in balance leaves. 
 });
@@ -175,5 +273,7 @@ module.exports = {
     createEmployeesLeave,
     addLeaveRequest,
     showLeavesDetails,
-    approveLeaves
+    approveLeaves,
+    multipleApproveLeaves,
+    showRequestStatusLeaves
 }
--- backend/routes/routeLeave.js
+++ backend/routes/routeLeave.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { createEmployeesLeave, addLeaveRequest, showLeavesDetails } = require("../controllers/leave_controller");
+const { createEmployeesLeave, 
+    addLeaveRequest, 
+    showLeavesDetails, 
+    approveLeaves, 
+    multipleApproveLeaves,
+    showRequestStatusLeaves
+     } = require("../controllers/leave_controller");
 const router = express.Router();
 
 // Admin
@@ -8,6 +14,27 @@ router.route("/CreateLeaves").post(createEmployeesLeave);
 // Employee
 router.route("/AddRequest").post(addLeaveRequest);
 router.route("/ShowDetails").get(showLeavesDetails);
-//router.route("/Show-Details/:leaveId").get(showLeavesDetails);
+router.route("/ApproveLeaves/:leaveId/:employeeLeaveId").post(approveLeaves);
+router.route("/ApproveLeaves").post(multipleApproveLeaves);
+router.route("/ShowReqStatusLeaves").get(showRequestStatusLeaves);
+
+
+/*
+Note ->>>>>>> parameters and queryString in route.
+1st Parameter URL :->
+http://localhost:6060/api/v1/leaves/ApproveLeaves/1 -> it means will send the parameter with the url.
+route should for parameter -> router.route("/ApproveLeaves/:leaveId").post(approveLeaves);
+in code we get param value from req.param
+
+2nd Query string URL :->
+http://localhost:6060/api/v1/leaves/ApproveLeaves/?leaveId:1 -> it means will send the queryString.
+route should be this -> router.route("/ApproveLeaves").post(approveLeaves);
+In code we get query value from req.query.
+
+----->>> This is the difference b/w in Query and Params
+Parms -> using parameter name in route but not in url. Just pass the value.
+Query -> not query to add any parameter in route. But in url pass the value with the name of value we can say key value pair (?leaveId=1)
+*/
+
 
 module.exports = router;
